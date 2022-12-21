6b6993bd0090aabfde074004dccb7fae5c32f95c
sayed <sayed@SAYED>
2022-12-21 15:17:08 +0600

    Add project files.

---
+++ LoginCrud/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace LoginCrud.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;    
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }    
+
+    }
+}
